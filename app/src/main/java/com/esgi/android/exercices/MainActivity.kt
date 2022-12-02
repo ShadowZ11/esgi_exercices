491df9d04d3b12f4674ccb040113a8ef57629655
ShadowZ11 <manuboisset@gmail.com>
2022-12-02 17:21:48 +0100

    Ajout list

--- app/src/main/java/com/esgi/android/exercices/MainActivity.kt
+++ app/src/main/java/com/esgi/android/exercices/MainActivity.kt
@@ -2,12 +2,17 @@ package com.esgi.android.exercices
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+
+        val name = findViewById<TextView>(R.id.name)
+
         // TODO Changer cette ligne en fonction des layouts
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.layout_detail)
+        name.text = "Emmanuel"
     }
 }
